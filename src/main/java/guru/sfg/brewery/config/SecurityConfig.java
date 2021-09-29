cd1c758fee7e8ed28eddf7121d288e64731959a6
FrankyBBoy <francis1592@gmai.com>
2021-09-29 13:16:59 -0400

    Section 6 - Password Encoding

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.springframework.http.HttpMethod.GET;
-
+/**
+ * Created by jt on 6/13/20.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,34 +22,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .authorizeRequests(authorize -> {
           authorize
-              .antMatchers("/", "/webjars/**", "/login",
-                  "/resources/**", "/beers/find", "/beers*").permitAll()
-              .antMatchers(GET, "/api/v1/beer/**").permitAll()
-              .mvcMatchers(GET, "/api/v1/beerUpc/{upc}").permitAll();
+              .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+              .antMatchers("/beers/find", "/beers*").permitAll()
+              .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+              .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
         })
         .authorizeRequests()
-        .anyRequest().authenticated().and()
+        .anyRequest().authenticated()
+        .and()
         .formLogin().and()
         .httpBasic();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
         .withUser("spring")
-        .password("{noop}guru")
+        .password("{bcrypt}$2a$10$7tYAvVL2/KwcQTcQywHIleKueg4ZK7y7d44hKyngjTwHCDlesxdla")
         .roles("ADMIN")
         .and()
         .withUser("user")
-        .password("{noop}password")
-        .roles("USER")
-        .and()
-        .withUser("scott")
-        .password("{noop}tiger")
-        .roles("CUSTOMER");
+        .password("{sha256}1296cefceb47413d3fb91ac7586a4625c33937b4d3109f5a4dd96c79c46193a029db713b96006ded")
+        .roles("USER");
+
+    auth.inMemoryAuthentication().withUser("scott").password("{bcrypt15}$2a$15$baOmQtw8UqWZRDQhMFPFj.xhkkWveCTQHe4OBdr8yw8QshejiSbI6").roles("CUSTOMER");
   }
 
-  //    @Override
+//    @Override
 //    @Bean
 //    protected UserDetailsService userDetailsService() {
 //        UserDetails admin = User.withDefaultPasswordEncoder()
@@ -66,5 +70,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //        return new InMemoryUserDetailsManager(admin, user);
 //    }
-
 }
