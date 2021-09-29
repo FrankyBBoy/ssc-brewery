c5951294b7b07d82bf522b96e7d2f2cc8a2299d6
FrankyBBoy <francis1592@gmai.com>
2021-09-29 11:47:04 -0400

    Lecture 29 - Config in memory authentication

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -25,6 +30,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated().and()
         .formLogin().and()
         .httpBasic();
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username("spring")
+        .password("guru")
+        .roles("ADMIN")
+        .build();
+
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user")
+        .password("password")
+        .roles("USER")
+        .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
   }
 
 }
