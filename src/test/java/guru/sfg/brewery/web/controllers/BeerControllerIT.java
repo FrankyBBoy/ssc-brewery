44384afe86f20fd9352059ee45bf6e24f5b5eaa1
FrankyBBoy <francis1592@gmai.com>
2021-09-29 11:53:45 -0400

    Lecture 30: In Memory Authentication Fluent API

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -14,7 +15,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class BeerControllerIT extends BaseIT{
 
-  @WithMockUser("spring")
+  @Test
+  void initCreationForm() throws Exception {
+    mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+        .andExpect(status().isOk())
+        .andExpect(view().name("beers/createBeer"))
+        .andExpect(model().attributeExists("beer"));
+  }
+
   @Test
   void findBeers() throws Exception{
     mockMvc.perform(get("/beers/find"))
@@ -24,8 +32,8 @@ public class BeerControllerIT extends BaseIT{
   }
 
   @Test
-  void findBeersWithHttpBasic() throws Exception{
-    mockMvc.perform(get("/beers/find"))
+  void findBeersWithAnonymous() throws Exception{
+    mockMvc.perform(get("/beers/find").with(anonymous()))
         .andExpect(status().isOk())
         .andExpect(view().name("beers/findBeers"))
         .andExpect(model().attributeExists("beer"));
